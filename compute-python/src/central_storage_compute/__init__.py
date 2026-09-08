7e87212914649dcf5acc1afeceb397567bd93de2
phasuwut <phasuwut.job@gmail.com>
2026-09-08 16:54:57 +0700

    fix(compute-client): point PyPI metadata to public source

--- compute-python/src/central_storage_compute/__init__.py
+++ compute-python/src/central_storage_compute/__init__.py
@@ -1,5 +1,5 @@
 from .client import ComputeApiError, ComputeClient, DownloadResult, UploadResult
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ["ComputeApiError", "ComputeClient", "DownloadResult", "UploadResult", "__version__"]
